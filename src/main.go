23e7782e661d37056531550f0cdb9a7104c407f5
Nolan <nolan.hellyer@gmail.com>
2020-05-04 23:15:07 -0700

    Refactor and make database store records.

--- src/main.go
+++ src/main.go
@@ -3,18 +3,52 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
+var fieldCounter = 0
+
 type postGreeting struct {
 	Greeting string `json:"greeting"`
+	Language string `json:"language"`
+	IsFormal bool   `json:"isFormal"`
+	NumWords int    `json:"numWords"`
 }
 
-var database = make([]string, 0, 10)
+type greetingRecord struct {
+	ID       int    `json:"id"`
+	Greeting string `json:"greeting"`
+	Language string `json:"language"`
+	IsFormal bool   `json:"isFormal"`
+	NumWords int    `json:"numWords"`
+}
+
+type createdResponse struct {
+	ID int `json:"id"`
+}
+
+func makeNewRecord(pg *postGreeting, gr *greetingRecord) {
+	gr.ID = fieldCounter
+	gr.Greeting = pg.Greeting
+	gr.Language = pg.Language
+	gr.IsFormal = pg.IsFormal
+	gr.NumWords = pg.NumWords
+
+	fieldCounter++
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+var database = make([]greetingRecord, 0, 10)
 
 func main() {
 	http.HandleFunc("/greeting", getGreetingHandlers)
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+
+	log.Fatal(err)
 }
 
 func getGreetingHandlers(w http.ResponseWriter, req *http.Request) {
@@ -27,25 +61,43 @@ func getGreetingHandlers(w http.ResponseWriter, req *http.Request) {
 }
 
 func getGreetings(w http.ResponseWriter, req *http.Request) {
-	bytes, err := json.Marshal(database)
-
-	if err != nil {
-		// TODO: Figure out how to set status code
-		fmt.Fprintf(w, "Internal Server Error")
-	} else {
-		res := string(bytes)
-		fmt.Fprintf(w, res)
-	}
+	encodeResponse(w, req, database, http.StatusOK)
 }
 
 func addGreeting(w http.ResponseWriter, req *http.Request) {
 	var newGreeting postGreeting
-	err := json.NewDecoder(req.Body).Decode(&newGreeting)
+	err := decodeRequestBody(req, &newGreeting)
 
 	if err != nil {
-		fmt.Fprintf(w, "Internal Server Error")
-	}
+		var res errorResponse = errorResponse{Error: "Internal Server Error"}
+		encodedRes, _ := json.Marshal(res)
 
-	database = append(database, newGreeting.Greeting)
-	fmt.Fprintf(w, "Created")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write(encodedRes)
+	} else {
+		var newRecord greetingRecord
+		makeNewRecord(&newGreeting, &newRecord)
+
+		response := createdResponse{ID: newRecord.ID}
+		database = append(database, newRecord)
+
+		encodeResponse(w, req, response, http.StatusCreated)
+	}
+}
+
+func decodeRequestBody(req *http.Request, i interface{}) error {
+	err := json.NewDecoder(req.Body).Decode(&i)
+	return err
+}
+
+func encodeResponse(w http.ResponseWriter, req *http.Request, i interface{}, code int) ([]byte, error) {
+	bytes, err := json.Marshal(i)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Internal Server Error")
+	} else {
+		w.WriteHeader(code)
+		w.Write(bytes)
+	}
+	return bytes, err
 }
