7c360f3c42205c1d48297e5a0ee85bf2e5bb9c0b
Nolan <nolan.hellyer@gmail.com>
2020-05-04 19:10:16 -0700

    Add greeting almost works

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nolwn/important-server
+
+go 1.14
--- src/main.go
+++ src/main.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type postGreeting struct {
+	greeting string
+}
+
+var database = make([]string, 0, 10)
+
 func main() {
-	http.HandleFunc("/deal", hello)
+	http.HandleFunc("/greeting", getGreetingHandlers)
 	http.ListenAndServe(":3000", nil)
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello")
+func getGreetingHandlers(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		getGreetings(w, req)
+	case http.MethodPost:
+		addGreeting(w, req)
+	}
+}
+
+func getGreetings(w http.ResponseWriter, req *http.Request) {
+	bytes, err := json.Marshal(database)
+
+	if err != nil {
+		// TODO: Figure out how to set status code
+		fmt.Fprintf(w, "Internal Server Error")
+	} else {
+		res := string(bytes)
+		fmt.Fprintf(w, res)
+	}
+}
+
+func addGreeting(w http.ResponseWriter, req *http.Request) {
+	var newGreeting postGreeting
+	err := json.NewDecoder(req.Body).Decode(&newGreeting)
+
+	if err != nil {
+		fmt.Fprintf(w, "Internal Server Error")
+	}
+
+	database = append(database, newGreeting.greeting)
+	fmt.Fprintf(w, "Created")
 }
